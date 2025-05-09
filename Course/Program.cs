b77ac0acff48098831f91bca9445b4ba78df504c
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-05-09 20:56:08 -0300

    First Commit

---
+++ Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Quantos números inteiros vc vai digitar? ");
+        int amount = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 1; i <= amount; i++)
+        {
+            Console.Write($"Valor #{i}: ");
+            sum += int.Parse(Console.ReadLine());
+        }
+
+        Console.Write($"Soma = {sum}");
+    }
+}
