1b62b7d629c77dead0b0f5f919a24fd0e99ecc1d
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-05-09 21:54:49 -0300

    Adicionando .gitignore para ignorar arquivos do Visual Studio

--- Course/Program.cs
+++ Course/Program.cs
@@ -4,16 +4,32 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.Write("Quantos números inteiros vc vai digitar? ");
-        int amount = int.Parse(Console.ReadLine());
-        int sum = 0;
+        double xA, xB, xC, yA, yB, yC;
 
-        for (int i = 1; i <= amount; i++)
-        {
-            Console.Write($"Valor #{i}: ");
-            sum += int.Parse(Console.ReadLine());
-        }
+        // Gets the measures of the triangle X
+        Console.WriteLine("Entre com as medidas do triangulo X: ");
+        xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-        Console.Write($"Soma = {sum}");
+        // Gets the measures of the triangle Y 
+        Console.WriteLine("Entre com as medidas do triangulo Y: ");
+        yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double p = (xA + xB + xC) / 2;
+        double xArea = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC) / 2;
+        double yArea = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        Console.WriteLine("Área de X = " + xArea.ToString("F4", CultureInfo.InvariantCulture));
+        Console.WriteLine("Área de Y = " + yArea.ToString("F4", CultureInfo.InvariantCulture));
+
+        if (xArea > yArea)
+            Console.WriteLine("Maior área: X");
+        else
+            Console.WriteLine("Maior área: Y");
     }
 }
