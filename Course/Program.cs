bf8005c516ba27f8384877a8a571dbb67aa3520a
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-07-10 19:28:54 -0300

    Course exercise, Product class exercise
    In this exercise, the program add and remove products showing the
    updated data about the product

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,7 +1,41 @@
-﻿internal class Program
+﻿using Course;
+using System.Globalization;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        Product product = new Product();
+
+        Console.WriteLine("Entre os dados do produto:");
         
+        Console.Write("Nome: ");
+        product.Name = Console.ReadLine();
+
+        Console.Write("Preço: ");
+        product.Price = double.Parse(Console.ReadLine()!);
+
+        Console.Write("Quantidade no estoque: ");
+        product.Quantity = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine($"\nDados do produto: {product.Name}," +
+            $" $ {product.Price.ToString("F2", CultureInfo.InvariantCulture)}," +
+            $" {product.Quantity} unidades," +
+            $" Total: $ {product.TotalInStock().ToString("F2", CultureInfo.InvariantCulture)}");
+
+        product.AddProduct();
+
+        Console.WriteLine($"\nDados atualizados: {product.Name}," +
+            $" $ {product.Price.ToString("F2", CultureInfo.InvariantCulture)}," +
+            $" {product.Quantity} unidades," +
+            $" Total: $ {product.TotalInStock().ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+        product.RemoveProduct();
+
+        Console.WriteLine($"\nDados atualizados: {product.Name}," +
+            $" $ {product.Price.ToString("F2", CultureInfo.InvariantCulture)}," +
+            $" {product.Quantity} unidades," +
+            $" Total: $ {product.TotalInStock().ToString("F2", CultureInfo.InvariantCulture)}");
     }
 }
