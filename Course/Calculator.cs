167d50e93829358144a428a7b53189bf83378c6a
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-07-14 21:14:44 -0300

    Course ex, learning about static members 2
    Now, a non-static calculator object has been created

---
+++ Course/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class Calculator
+    {
+        public double PI = 3.14;
+
+        public double Circumference(double radius)
+        {
+            return 2 * PI * radius;
+        }
+
+        public double Volume(double radius)
+        {
+            return (4.0 / 3.0) * PI * Math.Pow(radius, 3);
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -1,28 +1,19 @@
-﻿using System.Globalization;
+﻿using Course;
+using System.Globalization;
 
 internal class Program
 {
-    private static double PI = 3.14;
-
     private static void Main(string[] args)
     {
-        Console.WriteLine("Entre o valor do raio: ");
+        Calculator calculator = new Calculator();
+
+        Console.Write("Entre o valor do raio: ");
         double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-        double circ = Circumference(radius);
-        double volume = Volume(radius);
+        double circ = calculator.Circumference(radius);
+        double volume = calculator.Volume(radius);
         Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
         Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
-        Console.WriteLine("Valor de PI: " + PI.ToString("F2", CultureInfo.InvariantCulture));
-    }
-
-    private static double Circumference(double radius)
-    {
-        return 2 * PI * radius;
-    }
-
-    private static double Volume(double radius)
-    {
-        return (4.0 / 3.0) * PI * Math.Pow(radius, 3);
+        Console.WriteLine("Valor de PI: " + calculator.PI.ToString("F2", CultureInfo.InvariantCulture));
     }
 }
