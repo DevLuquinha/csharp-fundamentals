e8e5ff9acec17b4330114323e872bd08a46ba7da
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-08-05 22:01:41 -0300

    Fixing ex, Bank System very simple

---
+++ Course/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    public class BankAccount
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        
+        public BankAccount()
+        {
+            Balance = 0;
+        }
+
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public void WithDrow(double value)
+        {
+            Balance -= (value+5);
+        }
+        public override string ToString()
+        {
+            return $"Conta {Number}, Titular: {Holder}, Saldo: $ {Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -5,10 +5,35 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Product p = new Product("TV", 500, 10);
-        p.Name = "T";
+        BankAccount bankAccount = new BankAccount();
 
-        Console.WriteLine(p.Name);
-        Console.WriteLine(p.Price);
+        Console.Write("Entre o número da conta: ");
+        bankAccount.Number = int.Parse(Console.ReadLine());
+
+        Console.Write("Entre o titular da conta: ");
+        bankAccount.Holder = Console.ReadLine();
+
+        Console.Write("haverá depósito inicial (s/n)? ");
+        string hasInitialDeposit = Console.ReadLine();
+        if(hasInitialDeposit == "s")
+        {
+            Console.Write("Entre o valor de depósito inicial: ");
+            bankAccount.Deposit(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+        }
+
+        Console.WriteLine("\nDados da conta:");
+        Console.WriteLine(bankAccount.ToString());
+
+        Console.Write("Entre um valor para depósito: ");
+        bankAccount.Deposit(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+        Console.WriteLine("\nDados da conta atualizados:");
+        Console.WriteLine(bankAccount.ToString());
+
+        Console.Write("\nEntre um valor para saque: ");
+        bankAccount.WithDrow(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+        Console.WriteLine("Dados da conta atualizados:");
+        Console.WriteLine(bankAccount.ToString());
     }
 }
