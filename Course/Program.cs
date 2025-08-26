713926f4a5166f54f19adb4673e3f2c7393d3c8d
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-08-25 23:07:58 -0300

    Trainning the initialize a struct

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using Course;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        Point p;
+        p.X = 10;
+        p.Y = 20;
 
+        Console.WriteLine(p);
+        p = new Point();
+        Console.WriteLine(p);
     }
 }
