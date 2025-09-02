453f1187c1bac7b65d36146632d44ae364918179
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-09-02 20:47:52 -0300

    Ex lesson, calculate the average price of objects

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using Course;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 internal class Program
 {
@@ -6,18 +8,24 @@ internal class Program
     {
         int n = int.Parse(Console.ReadLine());
 
-        double[] vect = new double[n];
+        Product[] products = new Product[n];
 
-        for(int i = 0; i < vect.Length; i++)
-            vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        for(int i = 0; i < n; i++)
+        {
+            Product product = new Product();
+            product.Name = Console.ReadLine();
+            product.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            products[i] = product;
+        }
 
         double sum = 0.0;
 
-        for (int i = 0; i < vect.Length; i++)
-            sum += vect[i];
+        for (int i = 0; i < n; i++)
+            sum += products[i].Price;
 
         double avg = sum / n;
 
-        Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));
     }
 }
