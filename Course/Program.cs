0bd3359254e1189b41c9a9b4b779128afa1eae6c
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-09-15 22:49:26 -0300

    Fixing ex, Add Employee in list

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,27 +1,51 @@
-﻿using System.Globalization;
+﻿using Course;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        List<string> list = new List<string>();
-        list.Add("Lucas");
-        list.Add("Alex");
-        list.Add("Bob");
-        list.Add("Ana");
+        List<Employee> employees = new List<Employee>();
 
-        foreach (string s in list)
-            Console.WriteLine(s);
+        Console.Write("How many employees will be registered? ");
+        int employeeAmount = int.Parse(Console.ReadLine()); 
+        
+        for(int i = 0; i < employeeAmount; i++)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine($"Employee #{i+1}:");
+            
+            Console.Write("Id: ");
+            employee.Id = int.Parse(Console.ReadLine());
 
-        string s1 = list.Find(s => s[0] == 'A');
-        Console.WriteLine("First 'A': " + s1);
+            Console.Write("Name: ");
+            employee.Name = Console.ReadLine();
 
-        string s2 = list.FindLast(s => s[0] == 'A');
-        Console.WriteLine("Last 'A': " + s2);
+            Console.Write("Salary: ");
+            employee.Salary = double.Parse(Console.ReadLine());
 
-        List<string> listFiltered = list.FindAll(s => s.Length == 5);
-        Console.WriteLine("----------------------------------------");
-        foreach (string s in listFiltered)
-            Console.WriteLine(s);
+            employees.Add(employee);
+            Console.WriteLine();
+        }
+
+        Console.Write("Enter the employee id that will have salary increase: ");
+        int id = int.Parse(Console.ReadLine());
+
+        List<Employee> query = employees.FindAll(e => e.Id == id);
+        if (query.Count > 0)
+        {
+            Console.Write("Enter the percentage: ");
+            query[0].IncreaseSalary(double.Parse(Console.ReadLine()));
+        }
+        else
+        {
+            Console.WriteLine("This id does not exist!");
+        }
+
+        Console.WriteLine("\nUpdated list of employees:");
+        foreach (Employee employee in employees)
+        {
+            Console.WriteLine(employee.ToString());
+        }
+
     }
 }
