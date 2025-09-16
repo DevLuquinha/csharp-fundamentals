43432d595ebb45a7d63139bb845fba50bcf8fab3
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-09-15 22:12:52 -0300

    Lesson Ex, Learning about lists

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,45 +1,14 @@
-﻿using Course;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        int LEN = 10;
-        Room[] rooms = new Room[LEN];
+        List<string> list = new List<string>();
+        list.Add("Lucas");
+        list.Add("Alex");
+        list.Add("Bob");
+        list.Add("Ana");
 
-        for(int i = 0; i < LEN; i++)
-        {
-            rooms[i] = null;
-        }
-
-        Console.Write("How many rooms will be rented? ");
-        int numRooms = int.Parse(Console.ReadLine());
-
-        for(int i = 0; i < numRooms; i++)
-        {
-            Console.WriteLine("\nRENT #" + (i+1));
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Email: ");
-            string email = Console.ReadLine();
-            Console.Write("Room: ");
-            int id = int.Parse(Console.ReadLine());
-
-            rooms[id-1] = new Room()
-            {
-                Name = name,
-                Email = email,
-                Id = id
-            };
-        }
-
-        Console.WriteLine("\nBusy rooms: ");
-        for(int i = 0; i < LEN; i++)
-        {
-            if (rooms[i] != null)
-            {
-                Console.WriteLine(rooms[i].ToString());
-            }
-        }
+        foreach (string s in list)
+            Console.WriteLine(s);
     }
 }
