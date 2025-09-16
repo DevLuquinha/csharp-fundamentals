6c7251fc990d1a2355951bedb9751f9eabf9ca06
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-09-15 22:22:48 -0300

    Lesson Ex, testing the list methodss

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Globalization;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -9,6 +11,17 @@
         list.Add("Ana");
 
         foreach (string s in list)
+            Console.WriteLine(s);
+
+        string s1 = list.Find(s => s[0] == 'A');
+        Console.WriteLine("First 'A': " + s1);
+
+        string s2 = list.FindLast(s => s[0] == 'A');
+        Console.WriteLine("Last 'A': " + s2);
+
+        List<string> listFiltered = list.FindAll(s => s.Length == 5);
+        Console.WriteLine("----------------------------------------");
+        foreach (string s in listFiltered)
             Console.WriteLine(s);
     }
 }
