878b4b7a5f33ce1b6facf5b53d5bcdf63ca05739
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-12-26 16:48:13 -0300

    Clear the old classes

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,23 +1,10 @@
-﻿using Course.Entities;
-using Course.Entities.Enums;
-
-namespace Course
+﻿namespace Course
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Order order = new Order
-            {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-            };
-
-            Console.WriteLine(order.ToString());
-
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
-            Console.WriteLine(os);
+            
         }
     }
 }
