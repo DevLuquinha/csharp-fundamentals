4c23e63ee28402c026953e77c48b4b63af92dc96
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-12-26 17:54:14 -0300

    Create the HourContract class

---
+++ Course/HourContract.cs
@@ -0,0 +1,21 @@
+﻿namespace Course
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; }
+        public double ValuePerHour { get; }
+        public int Hours { get; }
+
+        public HourContract(
+            DateTime date, 
+            double valuePerHour,
+            int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue() => Hours * ValuePerHour;
+    }
+}
