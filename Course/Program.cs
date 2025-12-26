76e2eb788602ff80b25fea07248028e8e25c035c
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-12-26 16:36:33 -0300

    Learning about enums in C#

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,68 +1,20 @@
-﻿internal class Program
+﻿using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        string[] dimensions = Console.ReadLine().Split(" ");
-        
-        int lineDim = int.Parse(dimensions[0]);
-        int coluDim = int.Parse(dimensions[1]);
-        int[,] matriz = new int[lineDim, coluDim];
-
-        // Iterate over lines
-        for(int i = 0; i < matriz.GetLength(0); i++)
+        private static void Main(string[] args)
         {
-            string[] value = Console.ReadLine().Split(" ");
-           
-            // Iterate over columns
-            for (int j = 0; j < matriz.GetLength(1); j++)
+            Order order = new Order
             {
-                matriz[i, j] = int.Parse(value[j]);
-            }
-        }
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
 
-        // Choose number to find
-        int num = int.Parse(Console.ReadLine()!);
-
-        Console.WriteLine("==============================");
-        
-        for (int i = 0; i < matriz.GetLength(0); i++)
-        {
-            for (int j = 0; j < matriz.GetLength(1); j++)
-            {
-                double value = matriz[i, j];
-                
-                if (num == value)
-                {
-                    // Show the number position
-                    Console.WriteLine($"Position [{i},{j}]:");
-                    
-                    // Show the left number
-                    if(j - 1 >= 0)
-                    {
-                        Console.WriteLine($"Left: {matriz[i, j - 1]}");
-                    }
-                    
-                    // Show the right number
-                    if (j + 1 < matriz.GetLength(1))
-                    {
-                        Console.WriteLine($"Right: {matriz[i, j + 1]}");
-                    }
-
-                    // Show the up number
-                    if (i - 1 >= 0)
-                    {
-                        Console.WriteLine($"Up: {matriz[i - 1, j]}");
-                    }
-
-                    // Show the up number
-                    if (i + 1 < matriz.GetLength(0))
-                    {
-                        Console.WriteLine($"Down: {matriz[i + 1, j]}");
-                    }
-
-                    Console.WriteLine("==============================");
-                }
-            }
+            Console.WriteLine(order.ToString());
         }
     }
 }
