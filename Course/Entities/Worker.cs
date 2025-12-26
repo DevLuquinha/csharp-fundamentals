409e1c4acd55ad4012f0cf7bfa3d34ad188048be
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-12-26 17:53:56 -0300

    Create the Worker class

---
+++ Course/Entities/Worker.cs
@@ -0,0 +1,42 @@
+﻿using Course.Entities.Enums;
+
+namespace Course.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; }
+        public WorkerLevel Level { get; }
+        public double BaseSalary { get; }
+        
+        private readonly List<HourContract> _contracts;
+        public Worker(
+            string name, 
+            WorkerLevel level, 
+            double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+
+            _contracts = new List<HourContract>();
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            _contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            _contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            var contractsFiltered  = _contracts
+                .Where(c => c.Date.Year == year && c.Date.Month == month);
+
+            return contractsFiltered.Sum(c => c.TotalValue()) + BaseSalary;
+        }
+    }
+}
